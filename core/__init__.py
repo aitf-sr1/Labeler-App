1f5578897a026922b4f5faf4793341fa02d83f53
khaichi11 <adedhani03@gmail.com>
2026-04-23 22:52:42 +0700

    chore: initialize core and utils packages

---
+++ core/__init__.py
@@ -0,0 +1,3 @@
+from .inference import run_siglip_on_frames
+from .siglip_model import get_siglip
+from .face_detector import crop_face
---
+++ utils/__init__.py
@@ -0,0 +1,8 @@
+from .io import (
+    load_annotations, save_annotations,
+    load_flagged, save_flagged,
+    load_frame_annotations, save_frame_annotations,
+    load_batch_history, save_batch_history,
+    load_skipped, save_skipped
+)
+from .video import prepare_cropped_frames
